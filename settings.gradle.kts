818218931b352cef335192dcf0e4de7018a46349
Lance Rushing <lance@complyco.com>
2025-12-19 13:27:34 -0800

    Add github warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,10 +17,32 @@ dependencyResolutionManagement {
         google()
         mavenLocal()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-//        maven {
-//        url = uri("https://maven.pkg.github.com/ComplyCo/android-public")
-//        }
+        maven {
+            url = uri("https://jitpack.io")
+        }
+
+        val githubPassword: String =
+            providers.gradleProperty("gpr.key")
+                .orElse(System.getenv("GITHUB_TOKEN") ?: "")
+                .get()
+        if (githubPassword.isNotBlank()) {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/ComplyCo/android-public")
+                credentials {
+                    username = "x-access-token"
+                    password = githubPassword
+                }
+            }
+
+        } else {
+            println("#".repeat(70))
+            println("settings.gradle.kts: Missing GitHub Credentials. Github packages will be skipped.")
+            println("settings.gradle.kts: Recommend adding gpr.key=...PAT... to  ~/.gradle/gradle.properties.")
+            println("settings.gradle.kts: See: https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-gradle-registry")
+            println("#".repeat(70))
+        }
+
     }
 }
 
